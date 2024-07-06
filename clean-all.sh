0a3366d6b9d6e8830c6349ef9c7f6a8c13bf2f4b
Delyan Raychev <delyanr.com>
2024-07-06 15:31:53 -0700

    Remove tcp-demo namespace - separate tcp namespaces for client and server

--- clean-all.sh
+++ clean-all.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-for ns in bookbuyer bookstore bookthief bookwarehouse bookwatcher tcp-client tcp-echo-server tcp-demo; do
+for ns in bookbuyer bookstore bookthief bookwarehouse bookwatcher tcp-client tcp-echo-server; do
     kubectl delete namespace $ns --wait=true
     ## kubectl patch namespace $ns -p '{"spec":{"finalizers":[]}}' --type=merge
 done
-
--- deploy-meshy-bookstore.sh
+++ deploy-meshy-bookstore.sh
@@ -4,14 +4,14 @@ set -aueo pipefail
 
 # Source: https://release-v1-2.docs.openservicemesh.io/docs/getting_started/install_apps/
 
-for ns in bookbuyer bookstore bookthief bookwarehouse bookwatcher tcp-demo tcp-client tcp-echo-server; do
+for ns in bookbuyer bookstore bookthief bookwarehouse bookwatcher tcp-client tcp-echo-server; do
     kubectl create namespace $ns || true
     echo "Create the ${ns} service account and deployment:"
     kubectl apply -f https://raw.githubusercontent.com/draychev/meshy-bookstore/main/manifests/${ns}.yaml
-done    
+done
 
 
 echo "Checkpoint"
-for ns in bookbuyer bookstore bookthief bookwarehouse bookwatcher tcp-demo tcp-client tcp-echo-server; do
+for ns in bookbuyer bookstore bookthief bookwarehouse bookwatcher tcp-client tcp-echo-server; do
     kubectl get pods,deployments,serviceaccounts -n ${ns}
 done
